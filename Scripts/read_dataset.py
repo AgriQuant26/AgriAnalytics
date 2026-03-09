b851bf6acc89fb213ff73462c33dce20539ada9c
niharikavanka08 <vankaniharika08@gmail.com>
2026-03-09 20:20:04 +0000

    adding an input script to read the main dataset

---
+++ Scripts/read_dataset.py
@@ -0,0 +1,23 @@
+import kagglehub
+import os
+import pandas as pd
+
+# Download latest version
+path = kagglehub.dataset_download("akshatgupta7/crop-yield-in-indian-states-dataset")
+
+print("Path to dataset files:", path)
+
+# List all files in the downloaded path
+files = os.listdir(path)
+print(files)
+
+filename = "crop_yield.csv" 
+
+# 2. Create the full path to the file
+full_path = os.path.join(path, filename)
+
+# 3. Read it into a DataFrame
+df = pd.read_csv(full_path)
+
+# 4. Success! Check the data
+print(df.head())
